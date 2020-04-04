f60b7c722bf64763eb78fc54176596c1998d337a
kanKDA <apiecem9@gmail.com>
2020-04-04 18:58:41 +0400

    feat: quiz view

--- src/components/page-main/quiz/quiz.jsx
+++ src/components/page-main/quiz/quiz.jsx
@@ -3,21 +3,79 @@ import './quiz.pcss';
 
 import cn from '../../../utils/cn';
 
+const initState = {
+    expanded: false,
+    started: false,
+    qIndex: 0,
+    answers: [],
+    currentAnswer: ''
+}
+
+const answersStack = ['Да', 'Скорее да', 'Скорее нет', 'Нет', 'Не знаю'];
+
 @cn('quiz')
 class Quiz extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            expanded: false
+            expanded: false,
+            started: false,
+            finished: false,
+            qIndex: 0,
+            answers: [],
+            currentAnswer: '',
+            result: {}
         }
         this.expand.bind(this);
+        this.start.bind(this);
+        this.chooseAnswer.bind(this);
+        this.next.bind(this);
     }
+    quiz = this.props.quiz;
+    questions = this.quiz.questions;
     expand() {
         this.setState({expanded: !this.state.expanded});
         this.props.emitter();
     }
-    quiz = this.props.quiz;
+    start() {
+        this.setState({started: true});
+    }
+    close() {
+        this.setState(initState);
+    }
+    chooseAnswer(answer) {
+        this.setState({currentAnswer: answer});
+    }
+    next() {
+        if (this.questions[this.state.qIndex + 1]) {
+            const tmpAns = this.state.answers.slice(0);
+            const answerValue = 6 - (answersStack.indexOf(this.state.currentAnswer) + 1);
+            tmpAns.push(answerValue);
+            this.setState(prev => {
+                return {
+                    qIndex: prev.qIndex + 1,
+                    currentAnswer: '',
+                    answers: tmpAns
+                }
+            })
+        } else {
+            const sum = this.state.answers.reduce((i, p) => i + p);
+            const answer = this.quiz.results[Math.floor(sum / (this.quiz.results.length + 1))];
+            this.setState({
+                finished: true,
+                result: answer
+            })
+        }
+    }
     render(cn) {
+        console.log(this.state.result);
+        const question = this.questions[this.state.qIndex];
+        const answers = answersStack.map((item, i) => 
+        <div 
+        onClick={() => this.chooseAnswer(item)} 
+        className={ cn('answer', {active: item === this.state.currentAnswer}) } 
+        key={i}>{item}
+        </div>);
         return(
             <div className={ cn() }>
                 <div onClick={() => this.expand()} style={{backgroundImage: `url(${this.quiz.cover})`}} className={ cn('outer') }>
@@ -25,12 +83,31 @@ class Quiz extends React.Component {
                 </div>
                 <div style={{top: pageYOffset}} className={ cn('inner', {expanded: this.state.expanded}) }>
                     <div className={ cn('content') }>
-                        <h2 className={ cn('title', {expanded: true}) }>{this.quiz.title}</h2>
-                        <img className={ cn('cover') } src={this.quiz.cover}></img>
-                        <p className={ cn('font') }>{this.quiz.inner}</p>
-                        <div className={ cn('accept') }>Пройти тест</div>
+                        {!this.state.started ?
+                            <div className={ cn('preview-grid') }>
+                                <h2 className={ cn('title', {expanded: true}) }>{this.quiz.title}</h2>
+                                <img className={ cn('cover') } src={this.quiz.cover}></img>
+                                <p className={ cn('font') }>{this.quiz.inner}</p>
+                                <div onClick={() => this.start()} className={ cn('accept') }>Пройти тест</div>
+                            </div>
+                            : !this.state.finished ?
+                            <div className={ cn('quiz-grid') }>
+                                <h1 className={ cn('question-title') }>{question.title}</h1>
+                                <img className={ cn('cover', {q: true}) } src={question.cover}></img>
+                                <div className={ cn('answer-stack') }>
+                                    {answers}
+                                </div>
+                                {this.state.currentAnswer && <div onClick={() => this.next()} className={ cn('next') }>Дальше</div>}
+                            </div>
+                            :
+                            <div className={ cn('preview-grid')}>
+                                <h2 className={ cn('title', {expanded: true}) }>{this.state.result.title}</h2>
+                                <img className={ cn('cover') } src={this.state.result.cover}></img>
+                                <p className={ cn('font') }>{this.state.result.inner}</p>
+                            </div>
+                        }
                     </div>
-                    <div onClick={() => this.expand()} className={ cn('go-back') }></div>
+                    <div onClick={() => this.close()} className={ cn('go-back') }></div>
                 </div>
             </div>
         )
--- src/reducers/init.js
+++ src/reducers/init.js
@@ -1,43 +1,139 @@
 const quizes = [
     {
         title: 'Тестовый тест',
-        cover: './cover1.jpg',
+        cover: './static/cover1.jpg',
         inner: `"Разведопрос: Трудно быть русским" - книга о людях компетентных. Дмитрий Пучков беседует со специалистами - историками, журналистом, ополченцем и исследователем религий - грамотными вопросами направляя разговор в русло дачи толковых показаний.
         Историк Борис Юлин расскажет, как создавался Майдан и как за скачущими идеалистами следом приходят вооружённые экстремисты.
         Ополченец Николай сообщит, что видел своими глазами на Украине.
-        Историк Александр Скробач прояснит вопрос о Киевской Руси и древних украх.'`
+        Историк Александр Скробач прояснит вопрос о Киевской Руси и древних украх.'`,
+        questions: [
+            {
+                cover: './static/q1.jpg',
+                title: 'NAMFNklsa dK;AS JD?',
+            },
+            {
+                cover: './static/q2.jpg',
+                title: 'W aslkdj ODSJ lk j?',
+            },
+            {
+                cover: './static/q3.jpg',
+                title: 'Mhhhh?',
+            },
+            {
+                cover: './static/q4.jpg',
+                title: 'Owp pasdp x?',
+            }
+        ],
+        results: [
+            {
+                cover: './static/r1.jpg',
+                title: 'R1',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r2.jpg',
+                title: 'R2',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r3.jpg',
+                title: 'R3',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r4.jpg',
+                title: 'R4',
+                inner: 'aj askd;j ;asld qwm,. m'
+            }
+        ]
     },
     {
         title: 'Тестовый тест(1)',
-        inner: 'inner(1)'
+        inner: 'inner(1)',
+        questions: [
+            {
+                cover: './static/q1.jpg',
+                title: 'NAMFNklsa dK;AS JD?',
+            },
+            {
+                cover: './static/q2.jpg',
+                title: 'W aslkdj ODSJ lk j?',
+            },
+            {
+                cover: './static/q3.jpg',
+                title: 'Mhhhh?',
+            },
+            {
+                cover: './static/q4.jpg',
+                title: 'Owp pasdp x?',
+            }
+        ],
+        results: [
+            {
+                cover: './static/r1.jpg',
+                title: 'R1',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r2.jpg',
+                title: 'R2',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r3.jpg',
+                title: 'R3',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r4.jpg',
+                title: 'R4',
+                inner: 'aj askd;j ;asld qwm,. m'
+            }
+        ]
     },
     {
         title: 'Тестовый тест(2)',
-        inner: 'inner(2)'
-    },
-    {
-        title: 'Тестовый тест(3)',
-        inner: 'inner(3)'
-    },
-    {
-        title: 'Тестовый тест(4)',
-        inner: 'inner(4)'
-    },
-    {
-        title: 'Тестовый тест(5)',
-        inner: 'inner(5)'
-    },
-    {
-        title: 'Тестовый тест(6)',
-        inner: 'inner(7)'
-    },
-    {
-        title: 'Тестовый тест(8)',
-        inner: 'inner(8)'
-    },
-    {
-        title: 'Тестовый тест(9)',
-        inner: 'inner(9)'
+        inner: 'inner(2)',
+        questions: [
+            {
+                cover: './static/q1.jpg',
+                title: 'NAMFNklsa dK;AS JD?',
+            },
+            {
+                cover: './static/q2.jpg',
+                title: 'W aslkdj ODSJ lk j?',
+            },
+            {
+                cover: './static/q3.jpg',
+                title: 'Mhhhh?',
+            },
+            {
+                cover: './static/q4.jpg',
+                title: 'Owp pasdp x?',
+            }
+        ],
+        results: [
+            {
+                cover: './static/r1.jpg',
+                title: 'R1',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r2.jpg',
+                title: 'R2',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r3.jpg',
+                title: 'R3',
+                inner: 'aj askd;j ;asld qwm,. m'
+            },
+            {
+                cover: './static/r4.jpg',
+                title: 'R4',
+                inner: 'aj askd;j ;asld qwm,. m'
+            }
+        ]
     }
 ];
 
